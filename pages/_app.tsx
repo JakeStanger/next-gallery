100414f1d807aaba704723c8f7ce8a22b2e3bfb9
Jake Stanger <mail@jstanger.dev>
2021-01-10 14:06:04 +0000

    feat: sentry support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,23 @@ import '../styles/global.scss';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import './nprogress.scss';
 import '@stripe/stripe-js';
+import { Provider } from 'next-auth/client';
+import { init } from '../lib/sentry';
+
+init();
 
 // progress bar events
 router.events.on('routeChangeStart', () => NProgress.start());
 router.events.on('routeChangeComplete', () => NProgress.done());
 router.events.on('routeChangeError', () => NProgress.done());
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+  const err = (props as any).err;
+
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} err={err} />
+    </Provider>
+  );
 }
