c9efaae065d0bd005f8dd623b32cfc1395e54607
Jake Stanger <mail@jstanger.dev>
2021-01-11 19:38:47 +0000

    fix(docker): not building

--- .docker/gallery/entrypoint.sh
+++ .docker/gallery/entrypoint.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env sh
 
-set -euf -o
+set -euf
 
-if [ ! -d ".next" ]; then
+# check if .next does not exist or is empty
+if [ ! -d ".next" ] || [ -z "$(ls -A .next)" ]; then
+  echo 'No build found - performing one now'
   yarn build
 
   if [ -n "$AWS_S3_BUCKET_NAME" ]; then
+    echo 'Publishing static assets to AWS'
     yarn publish:aws
   fi
 fi
 
+echo "Starting server"
 yarn start
