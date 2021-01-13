3567838960f80e6d14ecbfa4136858dcbf24b0f5
Jake Stanger <mail@jstanger.dev>
2021-01-13 21:52:27 +0000

    feat: show loading label for full images

--- components/image/Image.tsx
+++ components/image/Image.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import NextImage from 'next/image';
 import IImageProps from './IImageProps';
 import useImageSrc from '../../lib/utils/useImageSrc';
+import styles from './Image.module.scss';
 
 const Image: React.FC<IImageProps> = ({ imageId, full, ...props }) => {
   const src = useImageSrc(imageId, full);
-  return <NextImage {...props} src={src} />;
+
+  if (!full) {
+    return <NextImage {...props} src={src} />;
+  } else {
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+       const img = containerRef.current?.querySelector(
+          'img[src^="/_next/image"]'
+        ) as HTMLImageElement;
+        img.addEventListener('load', () => {
+          setLoading(false);
+        });
+    }, []);
+
+    return (
+      <div ref={containerRef} className={styles.imgContainer}>
+        <NextImage {...props} src={src} loading={'eager'} />
+        {loading && <div className={styles.loadingIndicator}>Loading Image...</div>}
+      </div>
+    );
+  }
 };
 
 export default Image;
