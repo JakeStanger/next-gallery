ab38ec1dedd04be789a997b294e92201f0bbc022
Jake Stanger <mail@jstanger.dev>
2021-01-18 20:41:44 +0000

    fix(api): do not secure endpoints from localhost

--- lib/api/middleware/secure.ts
+++ lib/api/middleware/secure.ts
@@ -2,6 +2,10 @@ import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/client'
 
 const secure = (handler: NextApiHandler) => async (req: NextApiRequest, res: NextApiResponse) => {
+  if(req.headers.host?.startsWith('localhost')) {
+    return handler(req, res);
+  }
+
   const session = await getSession({ req });
   if(!session) {
     return res.status(401).end('Not authorized');
