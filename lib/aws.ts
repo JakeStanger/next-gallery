32ab1629743e6e11d48c4d576652bb4fd2da0271
Jake Stanger <mail@jstanger.dev>
2021-01-24 22:54:20 +0000

    fix(aws): use signature version v4

--- lib/aws.ts
+++ lib/aws.ts
@@ -1,5 +1,5 @@
 import AWS from 'aws-sdk';
 
 AWS.config.update({ region: process.env.AWS_REGION });
-const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+const s3 = new AWS.S3({ apiVersion: '2006-03-01', signatureVersion: 'v4' });
 export default s3;
