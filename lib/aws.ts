7da071852ffd35e32746639902b02e874f2c2667
Jake Stanger <mail@jstanger.dev>
2021-01-24 23:34:02 +0000

    fix(aws): vercel still using wrong region

--- lib/aws.ts
+++ lib/aws.ts
@@ -1,5 +1,8 @@
 import AWS from 'aws-sdk';
 
-AWS.config.update({ region: process.env.AWS_S3_REGION });
-const s3 = new AWS.S3({ apiVersion: '2006-03-01', signatureVersion: 'v4' });
+const s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  signatureVersion: 'v4',
+  region: process.env.AWS_S3_REGION,
+});
 export default s3;
