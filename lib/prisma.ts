5779807059f4aa919974bb8080f34a9d15400896
Jake Stanger <mail@jstanger.dev>
2022-03-19 00:03:27 +0000

    refactor(prisma): better client instance handling

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+declare global {
+  var prisma: PrismaClient;
+}
+
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+
 export default prisma;
