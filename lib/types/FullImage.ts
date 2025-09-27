b8d02a47b4b5ff1768f122e1c04d1a823bbd90f7
Jake Stanger <mail@jstanger.dev>
2025-09-27 16:00:32 +0100

    fix: type error
    why is the compiler not flagging these

--- lib/types/FullImage.ts
+++ lib/types/FullImage.ts
@@ -1,6 +1,7 @@
 import { Category, Group, Image, Location, PriceGroup, Tag } from '@prisma/client';
 
 interface FullImage extends Image {
+  name: string;
   group: Group | null;
   categories: Category[];
   priceGroup: PriceGroup | null;
